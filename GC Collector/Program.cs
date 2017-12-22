95ff2c6082bd75ed1297c1eb8ca12cee78f1a02e
Зейтын Бахытбек Кылышбекович <@corp.itstep>
2017-12-22 17:21:20 +0600

    first

---
+++ GC Collector/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC_Collector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            random.Name = "great";
+            var generation = GC.GetGeneration(random);
+
+            GC.Collect(generation);
+
+            Console.ReadKey();
+        }
+    }
+}
